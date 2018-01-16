a40d79ffac6eff328091d49c4fb4176a4a6bc8ff
William Fisher <ashton0312@gmail.com>
2018-01-15 20:45:14 -0500

    Added rocket thrust sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource rocketAudio;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = gameObject.GetComponent<Rigidbody>();
+        rocketAudio = gameObject.GetComponent<AudioSource>();
 	}
 
 
@@ -23,6 +25,14 @@ public class Rocket : MonoBehaviour {
         if(Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if(!rocketAudio.isPlaying)
+            {
+                rocketAudio.Play();
+            }
+        }
+        else
+        {
+            rocketAudio.Stop();
         }
         if(Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
